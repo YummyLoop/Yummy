64786914bfd1252b2e5dfa7476d318cb33e31e31
João <12478030+YummyLoop@users.noreply.github.com>
2019-08-15 04:39:33 +0100

    Refactor custom item class

--- src/main/java/yummyloop/example/item/Item.java
+++ src/main/java/yummyloop/example/item/Item.java
@@ -15,32 +15,32 @@ public class Item extends net.minecraft.item.Item {
         super(new net.minecraft.item.Item.Settings().group(ItemGroup.MISC));
     }
 
-    public Item(int maxDamage, int maxCount, net.minecraft.item.Item recipeRemainder, ItemGroup group, net.minecraft.util.Rarity rarity, net.minecraft.item.FoodComponent foodComponent) {
-        super(new net.minecraft.item.Item.Settings().maxDamage(maxDamage).maxCount(maxCount).recipeRemainder(recipeRemainder).group(group.getGroup()).rarity(rarity).food(foodComponent));
-    }
- 
-    public Item(int maxDamage, int maxCount, ItemGroup group) {
-        super(new net.minecraft.item.Item.Settings().maxDamage(maxDamage).maxCount(maxCount).group(group.getGroup()));
+    public Item(String modid, String itemName){
+        this();
+        register(modid, itemName);
     }
 
     public Item(ItemGroup group) {
         super(new net.minecraft.item.Item.Settings().group(group.getGroup()));
     }
 
-    public Item(String modid, String itemName){
-        super(new net.minecraft.item.Item.Settings().group(ItemGroup.MISC));
-        register(modid, itemName);
-    }
-
     public Item(String modid, String itemName, ItemGroup group) {
-        super(new net.minecraft.item.Item.Settings().group(group.getGroup()));
+        this(group);
+        register(modid, itemName);
+    }
+    
+    public Item(String modid, String itemName, ItemGroup group, int maxCount) {
+        super(new net.minecraft.item.Item.Settings().group(group.getGroup()).maxCount(maxCount));
         register(modid, itemName);
     }
 
+    public Item(int maxDamage, int maxCount, net.minecraft.item.Item recipeRemainder, ItemGroup group, net.minecraft.util.Rarity rarity, net.minecraft.item.FoodComponent foodComponent) {
+        super(new net.minecraft.item.Item.Settings().maxDamage(maxDamage).maxCount(maxCount).recipeRemainder(recipeRemainder).group(group.getGroup()).rarity(rarity).food(foodComponent));
+    }
+
+    // End of constructors 
     public boolean register (String modid, String itemName) {
         Registry.register(Registry.ITEM, new Identifier(modid, itemName), this);
         return true;
     }
-    
-
 }
