7ec9688857f7fdd5738fabd7262bc24c9e673a9e
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-08-16 16:29:00 +0100

    Convert identation to spaces

--- src/main/java/yummyloop/example/ExampleMod.java
+++ src/main/java/yummyloop/example/ExampleMod.java
@@ -6,15 +6,15 @@ import yummyloop.example.item.ItemGroup;
 
 public class ExampleMod implements ModInitializer {
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		
-		ItemGroup a = new ItemGroup("tutorial", "hello");
-		new Item("tutorial", "fabric_item", a);
-		
-		System.out.println("Hello Fabric world!");
-	}
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+        
+        ItemGroup a = new ItemGroup("tutorial", "hello");
+        new Item("tutorial", "fabric_item", a);
+        
+        System.out.println("Hello Fabric world!");
+    }
 }
