552e93cc0ff07bb38f24155634074fb809463d39
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-08-21 20:18:45 +0100

    Add logger class to facilitate the usage of the default logger(log4j2)

--- src/main/yummyloop/example/ExampleMod.kt
+++ src/main/yummyloop/example/ExampleMod.kt
@@ -3,20 +3,20 @@ package yummyloop.example
 import kotlinx.coroutines.runBlocking
 import net.fabricmc.api.ModInitializer
 import net.minecraft.block.Material
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
 import yummyloop.example.block.Block
 import yummyloop.example.config.Config
 import yummyloop.example.item.BlockItem
 import yummyloop.example.item.Item
 import yummyloop.example.item.ItemGroup
 import yummyloop.example.item.Items
+import yummyloop.example.util.Logger
 
 class ExampleMod : ModInitializer {
 
-    private val logger: Logger = LogManager.getFormatterLogger("KotlinLanguageTest")
+    private val logger: Logger = Logger("LoggerTest")
 
     override fun onInitialize() = runBlocking {
+        logger.setLevel("ALL")
         logger.info("**************************")
         logger.info("      Hello World !       ")
         logger.info("**************************")
@@ -25,7 +25,7 @@ class ExampleMod : ModInitializer {
         logger.trace("logger trace") // shows nothing
         logger.warn("logger warn")
         logger.fatal("logger fatal")
-
+        logger.trace("logger trace") // shows nothing
 
         Items()
         val itemGroup = ItemGroup("tutorial", "hello")
@@ -47,3 +47,21 @@ class ExampleMod : ModInitializer {
 
     }
 }
+/*
+    // import org.apache.logging.log4j.core.LoggerContext;
+    // import org.apache.logging.log4j.core.config.Configuration;
+    // import org.apache.logging.log4j.core.config.LoggerConfig;
+
+    LoggerContext context = (LoggerContext) LogManager.getContext(false);
+    Configuration config = context.getConfiguration();
+    LoggerConfig rootConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
+    rootConfig.setLevel(Level.DEBUG);
+
+    // You could also specify the actual logger name as below
+    // and it will return the LoggerConfig used by the Logger.
+    LoggerConfig loggerConfig = config.getLoggerConfig("com.apache.test");
+    loggerConfig.setLevel(Level.TRACE);
+
+    // This causes all Loggers to refetch information from their LoggerConfig.
+    context.updateLoggers();
+ */
---
+++ src/main/yummyloop/example/util/Logger.kt
@@ -0,0 +1,52 @@
+package yummyloop.example.util
+
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.core.config.Configurator
+
+// see https://logging.apache.org/log4j/log4j-2.4/faq.html
+class Logger(private val name: String) {
+
+    private val log: Logger = LogManager.getFormatterLogger(name)
+
+    fun setLevel(level : String){
+        val logLevel : Level = when (level) {
+            "ALL" -> Level.ALL
+            "DEBUG" -> Level.DEBUG
+            "ERROR" -> Level.ERROR
+            "FATAL" -> Level.FATAL
+            "INFO" -> Level.INFO
+            "OFF" -> Level.OFF
+            "TRACE" -> Level.TRACE
+            "WARN" -> Level.WARN
+            else -> Level.ALL
+        }
+        Configurator.setLevel(this.name, logLevel);
+        //Configurator.setRootLevel(logLevel);
+    }
+
+    fun debug(message : String){
+        this.log.debug(message)
+    }
+
+    fun error(message : String){
+        this.log.error(message)
+    }
+
+    fun fatal(message : String){
+        this.log.fatal(message)
+    }
+
+    fun info(message : String){
+        this.log.info(message)
+    }
+
+    fun trace(message : String){
+        this.log.trace(message)
+    }
+
+    fun warn(message : String){
+        this.log.warn(message)
+    }
+}
