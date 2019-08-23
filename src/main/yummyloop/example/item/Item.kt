0bb773a9b3a5a5bc3ef6dc71391877af18815b89
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-08-23 15:21:23 +0100

    Convert Item.java to kotlin

---
+++ src/main/yummyloop/example/item/Item.kt
@@ -0,0 +1,42 @@
+package yummyloop.example.item;
+
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+import net.minecraft.world.World
+import net.minecraft.item.Item as VanillaItem
+import net.minecraft.item.ItemGroup as VanillaItemGroup
+
+class Item(settings : Settings) : VanillaItem(settings) {
+    private val tooltip = ArrayList<Text>();
+
+    private constructor(group : VanillaItemGroup) :
+            this(Settings().group(group))
+    private constructor(group : ItemGroup) :
+            this(Settings().group(group.group))
+    constructor(modId : String, itemName: String) :
+            this(ItemGroup.MISC){ register(modId, itemName) }
+    constructor(modId : String, itemName : String, group : ItemGroup) :
+            this(group){ register(modId, itemName) }
+    constructor(modId : String, itemName : String, group : ItemGroup, maxCount : Int) :
+            this(Settings().group(group.group).maxCount(maxCount)){ register(modId, itemName) }
+
+    private fun register (modId : String, itemName : String) {
+        Registry.register(Registry.ITEM, Identifier(modId, itemName), this);
+    }
+
+    override fun appendTooltip(itemStack: ItemStack?, world: World?, tooltip: MutableList<Text>?, tooltipContext: TooltipContext?) {
+        tooltip?.addAll(this.tooltip);
+    }
+
+    /**
+     * Appends a tooltip to the item
+     * @param tooltip example: item.tutorial.fabric_item.tooltip
+     */
+    fun addTooltip(tooltip : String){
+        this.tooltip.add(TranslatableText(tooltip));
+    }
+}
