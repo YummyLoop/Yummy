63d609247faae5d930167d2bb50914bbe94a85f4
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-08-27 21:33:29 +0100

    Fix backpack dupe by locking the hotbar

--- src/main/yummyloop/example/item/backpack/Cont2.kt
+++ src/main/yummyloop/example/item/backpack/Cont2.kt
@@ -2,10 +2,12 @@ package yummyloop.example.item.backpack
 
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
-import net.minecraft.container.*
+import net.minecraft.container.Container
+import net.minecraft.container.ContainerType
+import net.minecraft.container.Slot
+import net.minecraft.container.SlotActionType
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
-import net.minecraft.inventory.BasicInventory
 import net.minecraft.inventory.Inventories
 import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
@@ -14,10 +16,10 @@ import net.minecraft.util.DefaultedList
 
 class Cont2(containerType : ContainerType<*>, int_1 : Int, private val playerInventory : PlayerInventory, private val inventory : Inventory, private val rows : Int) :
         Container(containerType , int_1) {
+    private val stack: ItemStack =  playerInventory.player.mainHandStack
 
-    override fun canUse(playerEntity_1: PlayerEntity): Boolean {
-        return this.inventory.canPlayerUseInv(playerEntity_1)
-    }
+    constructor(int_1 : Int, playerInventory_1 : PlayerInventory, inventory_1 : Inventory) :
+            this(ContainerType.GENERIC_9X6, int_1, playerInventory_1, inventory_1, 6)
 
     init { // Look at GenericContainer.java
         checkContainerSize(inventory, this.rows* 9)
@@ -30,7 +32,7 @@ class Cont2(containerType : ContainerType<*>, int_1 : Int, private val playerInv
         while (int_8 < this.rows) {
             column = 0
             while (column < 9) {
-                this.addSlot(Slot(inventory, column + int_8 * 9, 8 + column * 18, 18 + int_8 * 18))
+                this.addSlot(BoxSlot(inventory, column + int_8 * 9, 8 + column * 18, 18 + int_8 * 18))
                 ++column
             }
             ++int_8
@@ -50,9 +52,13 @@ class Cont2(containerType : ContainerType<*>, int_1 : Int, private val playerInv
         // Hot Bar
         int_8 = 0
         while (int_8 < 9) {
-            this.addSlot(Slot(playerInventory, int_8, 8 + int_8 * 18, 161 + int_3))
+            this.addSlot(HotBarSlot(playerInventory, int_8, 8 + int_8 * 18, 161 + int_3))
             ++int_8
         }
+    }
+
+    override fun canUse(playerEntity_1: PlayerEntity): Boolean {
+        return this.inventory.canPlayerUseInv(playerEntity_1)
     }
 
     override fun close(playerEntity_1: PlayerEntity) {
@@ -82,28 +88,29 @@ class Cont2(containerType : ContainerType<*>, int_1 : Int, private val playerInv
         return this.rows
     }
 
-
-
-
-
-    private val stack: ItemStack =  playerInventory.player.mainHandStack
-
-    constructor(int_1 : Int, playerInventory_1 : PlayerInventory, inventory_1 : Inventory) :
-            this(ContainerType.GENERIC_9X6, int_1, playerInventory_1, inventory_1, 6)
-    
     override fun onSlotClick(int_1: Int, int_2: Int, slotActionType: SlotActionType?, player: PlayerEntity?): ItemStack {
         //return if(int_1 > 0 && this.getSlot(int_1).stack == player?.getStackInHand(Hand.MAIN_HAND)) {
-        return if(int_1 > 0 && this.getSlot(int_1).stack.item == stack.item) {
+        //return if (int_1 > 0 && this.getSlot(int_1).stack.item is Backpack) {
+        return if (int_1 >= ((this.rows + 3) * 9) ) {// If its the HotBar
             ItemStack.EMPTY
         } else {
             super.onSlotClick(int_1, int_2, slotActionType, player)
         }
     }
 
-    /* Functions that seem useless
-    override fun canInsertIntoSlot(slot_1: Slot?): Boolean
-    override fun canInsertIntoSlot(itemStack_1: ItemStack?, slot_1: Slot?): Boolean
-    override fun canUse(player: PlayerEntity?): Boolean
-    override fun dropInventory(player: PlayerEntity?, world_1: World?, inventory: Inventory?)
-     */
+    private class BoxSlot(inventory_1: Inventory, int_1: Int, int_2: Int, int_3: Int) : Slot(inventory_1, int_1, int_2, int_3) {
+        override fun canInsert(itemStack_1: ItemStack): Boolean {
+            return itemStack_1.item !is Backpack
+        }
+    }
+    private class HotBarSlot(inventory_1: Inventory, int_1: Int, int_2: Int, int_3: Int) : Slot(inventory_1, int_1, int_2, int_3) {
+        override fun canInsert(itemStack_1: ItemStack): Boolean {
+            return itemStack_1.item !is Backpack
+        }
+
+        override fun canTakeItems(playerEntity_1: PlayerEntity?): Boolean {
+            return this.stack.item !is Backpack // gets stuck when this changes for some reason
+            //return false
+        }
+    }
 }
