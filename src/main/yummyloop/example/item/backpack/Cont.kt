5731402d43d1799deaeeb4f01f6629cfa539855e
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-08-27 08:10:28 +0100

    Code cleaning

--- src/main/yummyloop/example/item/backpack/Cont.kt
+++ src/main/yummyloop/example/item/backpack/Cont.kt
@@ -2,63 +2,31 @@ package yummyloop.example.item.backpack
 
 import net.minecraft.container.ContainerType
 import net.minecraft.container.GenericContainer
-import net.minecraft.container.Slot
 import net.minecraft.container.SlotActionType
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
 import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import net.minecraft.util.Hand
-import net.minecraft.world.World
-import yummyloop.example.item.Items
-import net.minecraft.item.Items as VanillaItems
 
-class Cont(containerType_1 : ContainerType<*>, int_1 : Int, playerInventory_1 : PlayerInventory, inventory_1 : Inventory, int_2 : Int) :
-        GenericContainer(containerType_1 , int_1, playerInventory_1,  inventory_1, int_2) {
+class Cont(containerType_1 : ContainerType<*>, int_1 : Int, playerInventory_1 : PlayerInventory, inventory_1 : Inventory, rows : Int) :
+        GenericContainer(containerType_1 , int_1, playerInventory_1,  inventory_1, rows) {
 
     constructor(int_1 : Int, playerInventory_1 : PlayerInventory, inventory_1 : Inventory) :
             this(ContainerType.GENERIC_9X6, int_1, playerInventory_1, inventory_1, 6)
 
-    /*
-    override fun canInsertIntoSlot(slot_1: Slot?): Boolean {
-        return slot_1?.stack?.item != Items.backpack
-    }
-
-    override fun canInsertIntoSlot(itemStack_1: ItemStack?, slot_1: Slot?): Boolean {
-        return itemStack_1?.item != Items.backpack
-    }
-*/
-    override fun onSlotClick(int_1: Int, int_2: Int, slotActionType_1: SlotActionType?, player: PlayerEntity?): ItemStack {
+    override fun onSlotClick(int_1: Int, int_2: Int, slotActionType: SlotActionType?, player: PlayerEntity?): ItemStack {
         return if(int_1 > 0 && this.getSlot(int_1).stack == player?.getStackInHand(Hand.MAIN_HAND)) {
             ItemStack.EMPTY;
         } else {
-            super.onSlotClick(int_1, int_2, slotActionType_1, player)
+            super.onSlotClick(int_1, int_2, slotActionType, player)
         }
     }
 
-/*
-    override fun transferSlot(player: PlayerEntity?, int_1: Int): ItemStack {
-        return if (player?.inventory?.cursorStack?.item == Items.backpack) {
-            ItemStack.EMPTY
-        } else {
-            super.transferSlot(player, int_1)
-        }
-    }
-
-    override fun dropInventory(player: PlayerEntity?, world_1: World?, inventory_1: Inventory?) {
-        if (player?.inventory?.cursorStack?.item == Items.backpack) {
-            return
-        } else {
-            super.dropInventory(player, world_1, inventory_1)
-        }
-    }
-
-    override fun canUse(player: PlayerEntity?): Boolean {
-        return if (player?.inventory?.cursorStack?.item == Items.backpack){
-            false
-        }else {
-            super.canUse(player)
-        }
-    }
-    */
+    /* Functions that seem useless
+    override fun canInsertIntoSlot(slot_1: Slot?): Boolean
+    override fun canInsertIntoSlot(itemStack_1: ItemStack?, slot_1: Slot?): Boolean
+    override fun canUse(player: PlayerEntity?): Boolean
+    override fun dropInventory(player: PlayerEntity?, world_1: World?, inventory_1: Inventory?)
+     */
 }
