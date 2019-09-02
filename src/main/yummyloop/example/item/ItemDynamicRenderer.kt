15de6a901656ec880a740e3a0a19c521cecd853a
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-02 17:58:26 +0100

    Refactor Custom ItemDynamicRenderer, removed redundant checks

--- src/main/yummyloop/example/item/ItemDynamicRenderer.kt
+++ src/main/yummyloop/example/item/ItemDynamicRenderer.kt
@@ -2,46 +2,42 @@ package yummyloop.example.item
 
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
-import net.minecraft.block.AbstractSkullBlock
-import net.minecraft.block.Blocks
-import net.minecraft.block.ChestBlock
-import net.minecraft.block.ShulkerBoxBlock
-import net.minecraft.block.entity.EnderChestBlockEntity
-import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher
-import net.minecraft.item.BannerItem
-import net.minecraft.item.BedItem
-import net.minecraft.item.BlockItem
+import net.minecraft.client.render.entity.model.ShieldEntityModel
 import net.minecraft.item.ItemStack
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
+import yummyloop.example.item.backpack.Backpack
 import net.minecraft.item.Items as VanillaItems
 
 @Environment(EnvType.CLIENT)
 object ItemDynamicRenderer {
-    val defaultRenders = listOf(BannerItem::class)
     fun render(stack: ItemStack, info: CallbackInfo){
-
-        // Don't render vanilla stuff
-        when (stack.item) {
-            is BannerItem -> return
-            is BedItem -> return
-            VanillaItems.SHIELD -> return
-            VanillaItems.TRIDENT -> return
-            Blocks.ENDER_CHEST.asItem() -> return
-            Blocks.TRAPPED_CHEST.asItem() -> return
-
-            is BlockItem -> when((stack.item as BlockItem).block){
-                is AbstractSkullBlock -> return
-                is ShulkerBoxBlock -> return
-                Blocks.CONDUIT -> return
-
-                // Vanilla default when not one of the above
-                is ChestBlock -> return
+        // Todo: Add custom item renders here
+        when (stack.item){
+            is Backpack ->  ShieldEntityModel().renderItem()
+            else -> {
+                return
             }
         }
-
-        // Todo: Add custom item renders here
-        BlockEntityRenderDispatcher.INSTANCE.renderEntity(EnderChestBlockEntity())
+        //BlockEntityRenderDispatcher.INSTANCE.renderEntity(EnderChestBlockEntity())
         info.cancel()
-
     }
 }
+
+// Vanilla Check
+/*when (stack.item) {
+    is BannerItem -> return
+    is BedItem -> return
+    VanillaItems.SHIELD -> return
+    VanillaItems.TRIDENT -> return
+    Blocks.ENDER_CHEST.asItem() -> return
+    Blocks.TRAPPED_CHEST.asItem() -> return
+
+    is BlockItem -> when((stack.item as BlockItem).block){
+        is AbstractSkullBlock -> return
+        is ShulkerBoxBlock -> return
+        Blocks.CONDUIT -> return
+
+        // Vanilla default when not one of the above
+        is ChestBlock -> return
+    }
+}*/
