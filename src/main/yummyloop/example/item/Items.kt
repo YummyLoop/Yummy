3893a9b37b2e4f4b3d7ac29a8fd50e9f98f3f1a0
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-04 09:32:35 +0100

    Refactor items declaration+storage

--- src/main/yummyloop/example/item/Items.kt
+++ src/main/yummyloop/example/item/Items.kt
@@ -5,22 +5,23 @@ import yummyloop.example.block.Block
 import yummyloop.example.block.entity.TemplateBlockWithEntity
 import yummyloop.example.item.backpack.Backpack
 import net.minecraft.item.Items as Vanilla
+import net.minecraft.item.Item as VanillaItem
 
 object Items {
-   @JvmField val groupA = ItemGroup("tutorial", "hello1", Vanilla.ANVIL)
-   //@JvmField val F = Item("tutorial", "fabric_item1", groupA)
-
-   @JvmField val itemX = Item("tutorial", "fabric_item", groupA)
+    @JvmField val itemList = mutableMapOf<String, VanillaItem>()
+    @JvmField val groupList = mutableMapOf<String, ItemGroup>()
 
    @JvmField val blockA = TemplateBlockWithEntity("tutorial", "example_blockrender", Block.of(Material.METAL).lightLevel(10))
-   @JvmField val blockItemA = BlockItem("tutorial", "example_blockrender", blockA)
-
-    @JvmField val backpack = Backpack("example", "ring", 6, groupA)
-    @JvmField val backpack2 = Backpack("example", "ring2", 5, groupA)
-    //val itemP = Item("example", "portal", groupA)
 
     init {
-        this.itemX.addTooltip("item.tutorial.fabric_item.tooltip")
+        groupList["groupA"] = ItemGroup("tutorial", "hello1", Vanilla.ANVIL)
+
+        itemList["itemX"] = Item("tutorial", "fabric_item", groupList["groupA"]!!)
+        itemList["backpack"] = Backpack("example", "ring", 6, groupList["groupA"]!!)
+        itemList["backpack2"] = Backpack("example", "ring2", 5, groupList["groupA"]!!)
+        itemList["blockItemA"] = BlockItem("tutorial", "example_blockrender", blockA)
+
+        (itemList["itemX"] as Item).addTooltip("item.tutorial.fabric_item.tooltip")
     }
 
 }
