50cb02212ec9958f1709c67604e52eb632b3c74a
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-08 14:12:46 +0100

    Fix leg rendering

--- src/main/yummyloop/example/client/render/entity/SpecialBipedEntityModel.kt
+++ src/main/yummyloop/example/client/render/entity/SpecialBipedEntityModel.kt
@@ -5,11 +5,8 @@ import net.minecraft.client.MinecraftClient
 import net.minecraft.client.model.Cuboid
 import net.minecraft.client.render.entity.model.BipedEntityModel
 import net.minecraft.client.render.model.json.ModelTransformation
-import net.minecraft.client.render.model.json.Transformation
-import net.minecraft.client.util.math.Vector3f
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.LivingEntity
-import kotlin.math.abs
 
 class SpecialBipedEntityModel<T : LivingEntity>(scale : Float, private val yRotationOffset : Float, textureWidth : Int, textureHeight : Int) : BipedEntityModel<T>(){
     constructor(): this(0.0f)
@@ -17,6 +14,7 @@ class SpecialBipedEntityModel<T : LivingEntity>(scale : Float, private val yRota
 
     companion object {
         private const val rad = 57.295776F
+        private var mirror = false
     }
 
     // Render method
@@ -25,72 +23,45 @@ class SpecialBipedEntityModel<T : LivingEntity>(scale : Float, private val yRota
 
         when (slot) {
             EquipmentSlot.HEAD -> {
-                renderA(player, head, scale, slot)
+                renderPart(player, head, scale, slot)
             }
             EquipmentSlot.CHEST -> {
-                //renderChest(player, slot)
-                renderA(player, body, scale, slot)
-                //renderA(player, rightArm, scale, slot)
+                //renderPart(player, body, scale, slot)
+                renderPart(player, rightArm, scale, slot)
 
             }
             EquipmentSlot.LEGS -> {
-                renderRightLeg(player, slot,true)
+                renderPart(player, rightLeg, scale, slot)
+                mirror=true
+                renderPart(player, leftLeg, scale, slot)
+                mirror=false
 
             }
             EquipmentSlot.FEET -> {
-                renderRightLeg(player, slot,true)
+                renderPart(player, rightLeg, scale, slot)
+                mirror=true
+                renderPart(player, leftLeg, scale, slot)
+                mirror=false
 
             }
             else -> {}
         }
-
         //println("pitch " + a.pitch + " roll " + a.roll + " yaw " + a.yaw)
         //println("x " + a.x + " y " + a.y + " z " + a.z)
         //println("rotation points: x " + a.rotationPointX + " y " + a.rotationPointY + " z " + a.rotationPointZ)
     }
 
-    private fun renderRightLeg(player: T, slot: EquipmentSlot, mirror: Boolean){
-        renderLeg(player, slot, rightLeg, false)
-        if (mirror) {
-            renderLeg(player, slot, leftLeg, true)
-        }
-    }
-
-    private fun renderLeg(player: T, slot: EquipmentSlot, part : Cuboid, isMirror: Boolean){
-        val mirror = if (isMirror) 1 else -1
-
-        GlStateManager.pushMatrix()
-
-        if (player.isInSneakingPose) {
-            GlStateManager.translatef(0.0f, 0.0f, 0.25f)
-        }
-
-        //Reference point / left / down / ?
-        GlStateManager.translatef(0F, 0.75F, 0F)
-        GlStateManager.rotatef(part.pitch* rad, 1f, 0f, 0f)
-        GlStateManager.rotatef(part.yaw* rad, 0f, 1f, 0f)
-        GlStateManager.rotatef(part.roll* rad, 0f, 0f, 1f)
-        GlStateManager.scalef(0.625f * mirror, -0.625f, 0.625f) // Default head scale
-
-        // Transformation - negate the effects of the head reference y
-        val transformation = Transformation(
-                Vector3f(0F,0F,0F), //rotation
-                Vector3f(0F,1.6F,0F), //translation / ?, up, ?
-                Vector3f(1F,1F,1F) //scale
-        )
-        ModelTransformation.applyGl(transformation,false)
-
-        MinecraftClient.getInstance().firstPersonRenderer.renderItem(player, player.getEquippedStack(slot), ModelTransformation.Type.HEAD)
-
-        GlStateManager.popMatrix()
-    }
-
-    private fun renderA(player: T, part : Cuboid, scale: Float, slot: EquipmentSlot) {
+    private fun renderPart(player: T, part : Cuboid, scale: Float, slot: EquipmentSlot) {
         glMatrix {
             GlStateManager.translatef(part.x, part.y, part.z)
 
             if (player.isInSneakingPose) {
-                GlStateManager.translatef(0.0f, 0.2f, 0.0f)
+                if(slot == EquipmentSlot.FEET || slot == EquipmentSlot.LEGS) {
+                    GlStateManager.translatef(0.0f,0.015f, 0f)
+                    part.pitch = part.pitch * 0.845F
+                }else{
+                    GlStateManager.translatef(0.0f, 0.2f, 0.0f)
+                }
             }
 
             if (part.pitch == 0.0f && part.yaw == 0.0f && part.roll == 0.0f) {
@@ -119,11 +90,15 @@ class SpecialBipedEntityModel<T : LivingEntity>(scale : Float, private val yRota
     private fun renderItem(player: T, part : Cuboid, scale: Float, slot: EquipmentSlot){
         glMatrix {
             // Fix part location
-            GlStateManager.translatef(-part.rotationPointX * scale, -part.rotationPointY * scale, -part.rotationPointZ * scale)
+            if (player.isInSneakingPose && (slot == EquipmentSlot.FEET || slot == EquipmentSlot.LEGS)) {
+                    GlStateManager.translatef(-part.rotationPointX * scale, -part.rotationPointY * scale, 0F)
+            } else {
+                GlStateManager.translatef(-part.rotationPointX * scale, -part.rotationPointY * scale, -part.rotationPointZ * scale)
+            }
 
             // Defaults
             GlStateManager.translatef(0F, -0.25F, 0F)
-            GlStateManager.scalef(-0.625f, -0.625f, 0.625f)
+            GlStateManager.scalef((if (mirror) -1 else 1 ) * -0.625f, -0.625f, 0.625f)
 
             MinecraftClient.getInstance().firstPersonRenderer.renderItem(player, player.getEquippedStack(slot), ModelTransformation.Type.HEAD)
         }
