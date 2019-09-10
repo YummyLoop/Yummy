4b2d97d4dc1c91116e96c82b24df0475c37160ed
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-10 19:21:03 +0100

    Minor simplification / removed redundant code

--- src/main/yummyloop/example/block/BlockWithEntity.kt
+++ src/main/yummyloop/example/block/BlockWithEntity.kt
@@ -4,27 +4,11 @@ import net.fabricmc.fabric.api.block.FabricBlockSettings
 import net.minecraft.block.BlockRenderType
 import net.minecraft.block.BlockState
 import net.minecraft.block.Material
-import net.minecraft.block.MaterialColor
-import net.minecraft.util.DyeColor
 import yummyloop.example.util.registry.RegistryManager
 import net.minecraft.block.BlockWithEntity as VanillaBlockWithEntity
 
 abstract class BlockWithEntity constructor(blockName: String, settings: FabricBlockSettings) :
         VanillaBlockWithEntity(settings.build()) {
-
-    companion object{
-        fun of(material: Material): FabricBlockSettings {
-            return FabricBlockSettings.of(material, material.color)
-        }
-
-        fun of(material: Material, color: MaterialColor): FabricBlockSettings {
-            return FabricBlockSettings.of(material, color)
-        }
-
-        fun of(material: Material, color: DyeColor): FabricBlockSettings {
-            return FabricBlockSettings.of(material, color.materialColor)
-        }
-    }
 
     init {
         register(blockName)
@@ -32,7 +16,7 @@ abstract class BlockWithEntity constructor(blockName: String, settings: FabricBl
 
     // BlockName
     constructor(blockName: String) :
-            this(blockName, of(Material.AIR))
+            this(blockName, Block.of(Material.AIR))
 
     // End of constructors
     private fun register(blockName: String) {
