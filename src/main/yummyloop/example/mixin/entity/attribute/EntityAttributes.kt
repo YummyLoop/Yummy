d9119da5a99a103dd056be503a7286fb0c09cc34
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-12 18:22:31 +0100

    Minor change

--- src/main/yummyloop/example/mixin/entity/attribute/EntityAttributes.kt
+++ src/main/yummyloop/example/mixin/entity/attribute/EntityAttributes.kt
@@ -11,11 +11,13 @@ import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.Mutable
 import org.spongepowered.asm.mixin.Shadow
 
+private val maxValue = Double.MAX_VALUE
+
 @Shadow @Final @Mutable private val MAX_HEALTH : EntityAttribute =
-        (ClampedEntityAttribute(null, "generic.maxHealth", 20.0, 0.0, Double.MAX_VALUE)).setName("Max Health").setTracked(true)
+        (ClampedEntityAttribute(null, "generic.maxHealth", 20.0, 0.0, maxValue)).setName("Max Health").setTracked(true)
 
 @Shadow @Final @Mutable private val ARMOR : EntityAttribute =
-        ClampedEntityAttribute(null, "generic.armor", 0.0, 0.0, Double.MAX_VALUE).setTracked(true)
+        ClampedEntityAttribute(null, "generic.armor", 0.0, 0.0, maxValue).setTracked(true)
 
 @Shadow @Final @Mutable private val ARMOR_TOUGHNESS : EntityAttribute =
-        (ClampedEntityAttribute(null, "generic.armorToughness", 0.0, 0.0, Double.MAX_VALUE)).setTracked(true);
+        (ClampedEntityAttribute(null, "generic.armorToughness", 0.0, 0.0, maxValue)).setTracked(true);
