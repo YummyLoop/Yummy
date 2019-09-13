38dc64e987197efd09f424b9911cbb21de05496f
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-13 14:33:43 +0100

    Modify how the changes to the hunger are made

--- src/main/yummyloop/example/mixin/entity/player/Hunger.kt
+++ src/main/yummyloop/example/mixin/entity/player/Hunger.kt
@@ -1,6 +1,8 @@
 package yummyloop.example.mixin.entity.player
 
 import net.minecraft.entity.player.HungerManager
+import net.minecraft.entity.player.PlayerEntity
+import org.spongepowered.asm.lib.Opcodes
 import org.spongepowered.asm.mixin.Mixin
 import org.spongepowered.asm.mixin.Shadow
 import org.spongepowered.asm.mixin.injection.At
@@ -15,17 +17,31 @@ class Hunger {
     @Shadow private var foodStarvationTimer: Int = 0
     @Shadow private var prevFoodLevel = 20
 
+    /**
+     * Replaces the default hunger calculation
+     * It removes the default max hunger limit (foodLevel=20)
+     */
+    /*
     @Inject(at = [At("HEAD")], method = ["add"], cancellable = true)
     private fun onHungerAdd(hunger : Int, saturationModifier : Float, info: CallbackInfo) {
         this.foodLevel = this.foodLevel + hunger
         this.foodSaturationLevel = (this.foodSaturationLevel + hunger.toFloat() * saturationModifier * 2.0f).coerceAtMost(this.foodLevel.toFloat())
         info.cancel()
+    }*/
+
+    /**
+     * Modify hunger calculation
+     */
+    @Inject(at = [At("FIELD", target = "Lnet/minecraft/entity/player/HungerManager;foodSaturationLevel:F", opcode = Opcodes.PUTFIELD)], method = ["add"], cancellable = true)
+    private fun onHungerAdd(hunger : Int, saturationModifier : Float, info: CallbackInfo) {
+        this.foodLevel = hunger + prevFoodLevel
     }
 
     /*
-    @Inject(at = [At("RETURN")], method = ["update"])
-    private fun onUpdate(playerEntity_1 : PlayerEntity , info: CallbackInfo) {
-        println("Hunger is $foodLevel")
+    @Inject(at = [At("HEAD")], method = ["update"])
+    private fun onUpdate(playerEntity_1 : PlayerEntity, info: CallbackInfo) {
+        if (foodLevel!=prevFoodLevel)
+        println("Hunger is $foodLevel & $foodSaturationLevel & $exhaustion & $foodStarvationTimer")
     }
     */
 }
