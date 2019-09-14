0c38bf0f592fe2f9ecc93081dc0637cbb32e4504
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-14 15:02:15 +0100

    Refactor specialarmor item constructors

--- src/main/yummyloop/example/item/armor/SpecialArmorItem.kt
+++ src/main/yummyloop/example/item/armor/SpecialArmorItem.kt
@@ -6,17 +6,14 @@ import net.minecraft.item.ArmorMaterials
 import net.minecraft.item.ItemGroup
 import yummyloop.example.item.ArmorItem
 
-abstract class SpecialArmorItem(
-        itemName: String,
-        armorMaterial : ArmorMaterial,
-        equipmentSlot : EquipmentSlot,
-        settings : Settings
-) :
-        ArmorItem(itemName, armorMaterial, equipmentSlot, settings), SpecialArmor {
-    constructor(itemName : String) :
-            this(itemName, ArmorMaterials.IRON, EquipmentSlot.HEAD)
-    constructor(itemName: String, armorMaterial : ArmorMaterial, equipmentSlot : EquipmentSlot) :
-            this(itemName, armorMaterial, equipmentSlot, Settings().group(ItemGroup.COMBAT))
+abstract class SpecialArmorItem : ArmorItem, SpecialArmor {
+
+    constructor(itemName: String, armorMaterial : ArmorMaterial, equipmentSlot : EquipmentSlot, settings : Settings)
+            : super(itemName, armorMaterial, equipmentSlot, settings)
+    constructor(itemName : String)
+            : this(itemName, ArmorMaterials.IRON, EquipmentSlot.HEAD)
+    constructor(itemName: String, armorMaterial : ArmorMaterial, equipmentSlot : EquipmentSlot)
+            : this(itemName, armorMaterial, equipmentSlot, Settings().group(ItemGroup.COMBAT))
 
     override fun addTooltip(tooltip : String) : SpecialArmorItem {
         super.addTooltip(tooltip)
