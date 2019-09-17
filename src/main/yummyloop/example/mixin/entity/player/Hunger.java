1127d48b7c0ae7ba9a78046ab451175b54982939
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-17 17:52:00 +0100

    Convert some mixins to java

---
+++ src/main/yummyloop/example/mixin/entity/player/Hunger.java
@@ -0,0 +1,44 @@
+package yummyloop.example.mixin.entity.player;
+
+import net.minecraft.entity.player.HungerManager;
+import org.spongepowered.asm.lib.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin({HungerManager.class})
+public final class Hunger {
+    @Shadow
+    private int foodLevel = 20;
+    @Shadow
+    private float foodSaturationLevel = 5.0F;
+    @Shadow
+    private float exhaustion;
+    @Shadow
+    private int foodStarvationTimer;
+    @Shadow
+    private int prevFoodLevel = 20;
+
+    @Inject(
+            at = @At(
+                    target = "Lnet/minecraft/entity/player/HungerManager;foodSaturationLevel:F",
+                    value = "FIELD",
+                    opcode = Opcodes.PUTFIELD
+            ),
+            method = "add",
+            cancellable = true
+    )
+    private void onHungerAdd(int hunger, float saturationModifier, CallbackInfo info) {
+        this.foodLevel = hunger + this.prevFoodLevel;
+    }
+
+    /*
+    @Inject(at = [At("HEAD")], method = ["update"])
+    private fun onUpdate(playerEntity_1 : PlayerEntity, info: CallbackInfo) {
+        if (foodLevel!=prevFoodLevel)
+        println("Hunger is $foodLevel & $foodSaturationLevel & $exhaustion & $foodStarvationTimer")
+    }
+    */
+}
--- src/main/yummyloop/example/mixin/item/Elytra.kt
+++
@@ -1,9 +0,0 @@
-package yummyloop.example.mixin.item
-
-import net.minecraft.item.DyeableItem
-import net.minecraft.item.ElytraItem
-import org.spongepowered.asm.mixin.Mixin
-
-@Mixin(ElytraItem::class)
-class Elytra : DyeableItem{
-}
