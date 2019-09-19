1dbeea6cce2729854978f9ae6b1d7bf7d22cd5ac
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2019-09-19 03:04:03 +0100

    moved some stuff

--- src/main/yummyloop/example/ExampleModClient.kt
+++ src/main/yummyloop/example/ExampleModClient.kt
@@ -1,76 +1,10 @@
 package yummyloop.example
 
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry
-import net.fabricmc.fabric.api.client.model.ModelResourceProvider
-import net.fabricmc.fabric.api.client.model.ModelVariantProvider
-import net.fabricmc.fabric.api.client.render.EntityRendererRegistry
-import net.minecraft.client.util.ModelIdentifier
-import net.minecraft.util.Identifier
-import yummyloop.example.item.Spear
-import yummyloop.example.item.models.SimpleBakedItemModel
-import yummyloop.example.render.models.UnbakedModel
 import yummyloop.example.util.registry.ClientManager
 
 object ExampleModClient : ClientModInitializer {
     override fun onInitializeClient() {
-
         ClientManager.ini()
-
-        //---------Code below is temporary, and/or needs to be improved
-
-        EntityRendererRegistry.INSTANCE.register(Spear.SpearEntity::class.java) { entityRenderDispatcher, context -> Spear.SpearEntityRenderer(entityRenderDispatcher) }
-
-
-        ModelLoadingRegistry.INSTANCE.registerAppender { manager, out->
-            println("--- ModelAppender called! ---")
-            out.accept(ModelIdentifier(ExampleMod.id,"model#custom")) }
-
-        ModelLoadingRegistry.INSTANCE.registerVariantProvider { manager->
-            ModelVariantProvider{
-                modelId, context->
-                if (modelId.variant == "custom" && modelId.namespace == ExampleMod.id) {
-                    println("--- ModelVariantProvider called! ---")
-                    context.loadModel(Identifier(ExampleMod.id, "custom"))
-                } else {
-                    null
-                }
-            }
-        }
-
-        ModelLoadingRegistry.INSTANCE.registerResourceProvider { manager->
-            ModelResourceProvider{
-                id, context->
-                when {
-                    id.toString() == "example:custom" -> context.loadModel(Identifier(ExampleMod.id, "custom2"))
-                    id.toString() == "example:custom2" -> {
-                        println("--- ModelResourceProvider called! ---")
-                        UnbakedModel(
-                                SimpleBakedItemModel(
-                                        ModelIdentifier("example", "template_be2"),
-                                        Identifier("example", "template_be2")
-                                )
-                        )
-                    }
-                    else -> null
-                }
-            }
-        }
-
-        /* // Check when model loads
-        ClientTickCallback.EVENT.register(
-                ClientTickCallback{
-                    client->
-                    try {
-                        if (client.bakedModelManager.getModel(ModelIdentifier("fabric:model#custom")) === bakedModel && bakedModel != null) {
-                            println("--- MODEL LOADED! ---")
-                        } else {
-                            println("--- MODEL NOT LOADED! ---")
-                        }
-                    } catch (e : NullPointerException){
-                        println("--- MODEL NOT LOADED! + null ---")
-                    }
-                }
-        )*/
     }
 }
--- src/main/yummyloop/example/util/registry/ClientManager.kt
+++ src/main/yummyloop/example/util/registry/ClientManager.kt
@@ -1,11 +1,16 @@
 package yummyloop.example.util.registry
 
+import net.fabricmc.fabric.api.client.model.ModelLoadingRegistry
+import net.fabricmc.fabric.api.client.model.ModelResourceProvider
+import net.fabricmc.fabric.api.client.model.ModelVariantProvider
 import net.fabricmc.fabric.api.client.render.BlockEntityRendererRegistry
 import net.fabricmc.fabric.api.client.render.ColorProviderRegistry
+import net.fabricmc.fabric.api.client.render.EntityRendererRegistry
 import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry
 import net.minecraft.client.color.item.ItemColorProvider
 import net.minecraft.client.gui.screen.ingame.AbstractContainerScreen
 import net.minecraft.client.render.block.entity.BlockEntityRenderer
+import net.minecraft.client.util.ModelIdentifier
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.DyeableItem
 import net.minecraft.item.ItemConvertible
@@ -13,6 +18,9 @@ import net.minecraft.util.Identifier
 import net.minecraft.util.PacketByteBuf
 import yummyloop.example.ExampleMod
 import yummyloop.example.item.Items
+import yummyloop.example.item.Spear
+import yummyloop.example.item.models.SimpleBakedItemModel
+import yummyloop.example.render.models.UnbakedModel
 import net.minecraft.block.entity.BlockEntity as VanillaBLockEntity
 import net.minecraft.item.Items as VanillaItems
 
@@ -81,5 +89,61 @@ object ClientManager {
         for (i in blockEntityRenderers){
             bindBlockEntityRenderer(i.key,i.value)
         }
+
+        //---------Code below is temporary, and/or needs to be improved
+
+        EntityRendererRegistry.INSTANCE.register(Spear.SpearEntity::class.java) { entityRenderDispatcher, context -> Spear.SpearEntityRenderer(entityRenderDispatcher) }
+
+
+        ModelLoadingRegistry.INSTANCE.registerAppender { manager, out->
+            println("--- ModelAppender called! ---")
+            out.accept(ModelIdentifier(ExampleMod.id,"model#custom")) }
+
+        ModelLoadingRegistry.INSTANCE.registerVariantProvider { manager->
+            ModelVariantProvider{
+                modelId, context->
+                if (modelId.variant == "custom" && modelId.namespace == ExampleMod.id) {
+                    println("--- ModelVariantProvider called! ---")
+                    context.loadModel(Identifier(ExampleMod.id, "custom"))
+                } else {
+                    null
+                }
+            }
+        }
+
+        ModelLoadingRegistry.INSTANCE.registerResourceProvider { manager->
+            ModelResourceProvider{
+                id, context->
+                when {
+                    id.toString() == "example:custom" -> context.loadModel(Identifier(ExampleMod.id, "custom2"))
+                    id.toString() == "example:custom2" -> {
+                        println("--- ModelResourceProvider called! ---")
+                        UnbakedModel(
+                                SimpleBakedItemModel(
+                                        ModelIdentifier("example", "template_be2"),
+                                        Identifier("example", "template_be2")
+                                )
+                        )
+                    }
+                    else -> null
+                }
+            }
+        }
+
+        /* // Check when model loads
+        ClientTickCallback.EVENT.register(
+                ClientTickCallback{
+                    client->
+                    try {
+                        if (client.bakedModelManager.getModel(ModelIdentifier("fabric:model#custom")) === bakedModel && bakedModel != null) {
+                            println("--- MODEL LOADED! ---")
+                        } else {
+                            println("--- MODEL NOT LOADED! ---")
+                        }
+                    } catch (e : NullPointerException){
+                        println("--- MODEL NOT LOADED! + null ---")
+                    }
+                }
+        )*/
     }
 }
