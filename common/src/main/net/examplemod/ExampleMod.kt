6abd4b51d6a3e4f34a087dcfe48e6a4f6aa43bbe
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2021-02-08 22:16:56 +0000

    Minor fix to the previous commit

--- common/src/main/net/examplemod/ExampleMod.kt
+++ common/src/main/net/examplemod/ExampleMod.kt
@@ -1,8 +1,9 @@
-package net.examplemod;
+package net.examplemod
 
 import me.shedaniel.architectury.registry.*
 import net.minecraft.block.Blocks
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
@@ -14,11 +15,11 @@ object ExampleMod {
     val REGISTRIES by lazyOf( Registries.get(MOD_ID))
 
     // Registering a new creative tab
-    var EXAMPLE_TAB = CreativeTabs.create(Identifier(MOD_ID, "example_tab")) { -> ItemStack(Blocks.COBBLESTONE) }
+    var EXAMPLE_TAB: ItemGroup = CreativeTabs.create(Identifier(MOD_ID, "example_tab")) { ItemStack(Blocks.COBBLESTONE) }
 
     // Registering a new item
     var ITEMS = DeferredRegister.create(MOD_ID, Registry.ITEM_KEY)
-    var EXAMPLE_ITEM = ITEMS.register("example_item") { -> Item(Item.Settings().group(EXAMPLE_TAB))}
+    var EXAMPLE_ITEM: RegistrySupplier<Item> = ITEMS.register("example_item") { Item(Item.Settings().group(EXAMPLE_TAB))}
     
     fun init() {
         ITEMS.register()
