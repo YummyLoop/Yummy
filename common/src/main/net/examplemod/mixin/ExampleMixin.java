12facfd44b4927162ede3339c452c3f09548c4f4
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2021-02-09 07:10:20 +0000

    Add example mixin

--- common/src/main/net/examplemod/mixin/ExampleMixin.java
+++ common/src/main/net/examplemod/mixin/ExampleMixin.java
@@ -1,11 +1,10 @@
-package yummyloop.yummy.mixin;
+package net.examplemod.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 
 @Mixin(TitleScreen.class)
 public final class ExampleMixin {
@@ -14,7 +13,6 @@ public final class ExampleMixin {
             method = "init()V"
     )
     private void init(CallbackInfo info) {
-        String var2 = "This line is printed by an example mod mixin!";
-        System.out.println(var2);
+        System.out.println("This line is printed by an example mod mixin!");
     }
 }
