b4f1b51b8a4591a035950b0db3ec48327e798445
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2021-02-12 06:06:20 +0000

    Add logger

--- common/src/main/net/examplemod/ExampleMod.kt
+++ common/src/main/net/examplemod/ExampleMod.kt
@@ -4,16 +4,19 @@ import me.shedaniel.architectury.registry.CreativeTabs
 import me.shedaniel.architectury.registry.DeferredRegister
 import me.shedaniel.architectury.registry.Registries
 import me.shedaniel.architectury.registry.RegistrySupplier
+import net.examplemod.config.clothconfig.AutoConfig
 import net.minecraft.block.Blocks
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
-import net.examplemod.config.clothconfig.AutoConfig
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
 
 object ExampleMod {
     const val MOD_ID = "yummy" // when changing this forge.toml needs to be changed too
+    val log: Logger = LogManager.getLogger("Yummy")
     val modConfig = AutoConfig()
 
     // We can use this if we don't want to use DeferredRegister
@@ -27,8 +30,14 @@ object ExampleMod {
     var EXAMPLE_ITEM: RegistrySupplier<Item> = ITEMS.register("example_item") { Item(Item.Settings().group(EXAMPLE_TAB))}
     
     fun init() {
+        log.info("**************************")
+        log.info("     YummY says hello!    ")
+        log.info("**************************")
+        log.error("logger error")
+        log.warn("logger warn")
+        log.fatal("logger fatal")
+
         ITEMS.register()
         println(ExampleExpectPlatform.getConfigDirectory().absolutePath)
-        println("Kotlin says hello !!!")
     }
 }
