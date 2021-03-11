6511e33aa14de3384559553335199925591beb8d
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2021-03-11 07:58:11 +0000

    Refactor tooltip

--- common/src/main/yummyloop/yummy/ModContent.kt
+++ common/src/main/yummyloop/yummy/ModContent.kt
@@ -1,38 +1,24 @@
 package yummyloop.yummy
 
-import com.mojang.blaze3d.systems.RenderSystem
 import io.netty.buffer.Unpooled
 import me.shedaniel.architectury.event.events.GuiEvent
 import me.shedaniel.architectury.event.events.PlayerEvent
-import me.shedaniel.architectury.event.events.TooltipEvent
-import me.shedaniel.architectury.event.events.client.ClientScreenInputEvent
 import me.shedaniel.architectury.networking.NetworkManager
 import me.shedaniel.architectury.registry.BlockProperties
 import net.minecraft.block.Block
 import net.minecraft.block.Material
 import net.minecraft.client.MinecraftClient
-import net.minecraft.client.gui.DrawableHelper
-import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.gui.screen.ingame.InventoryScreen
 import net.minecraft.client.gui.widget.TexturedButtonWidget
-import net.minecraft.client.item.TooltipContext
-import net.minecraft.client.util.InputUtil
-import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.EquipmentSlot
 import net.minecraft.entity.SpawnGroup
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.inventory.Inventory
 import net.minecraft.inventory.SimpleInventory
 import net.minecraft.item.ArmorMaterials
 import net.minecraft.item.BlockItem
-import net.minecraft.item.ItemStack
-import net.minecraft.nbt.CompoundTag
 import net.minecraft.network.PacketByteBuf
 import net.minecraft.server.network.ServerPlayerEntity
-import net.minecraft.text.OrderedText
-import net.minecraft.text.Text
-import net.minecraft.text.TranslatableText
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Identifier
 import yummyloop.test.block.BoxScreen
@@ -46,6 +32,7 @@ import yummyloop.test.geckolib.PotatoArmor2
 import yummyloop.test.gui.Factory1
 import yummyloop.test.gui.Screen1
 import yummyloop.test.gui.ScreenHandler1
+import yummyloop.yummy.client.ToolTipPreview
 import yummyloop.yummy.client.gui.screen.addWidget
 import yummyloop.yummy.integration.geckolib.GeckoUtils
 import yummyloop.yummy.items.Ytem
@@ -53,10 +40,8 @@ import yummyloop.yummy.items.YtemGroup
 import yummyloop.yummy.items.baa.Ba
 import yummyloop.yummy.items.baa.BaHandler
 import yummyloop.yummy.items.baa.BaScreen
-import yummyloop.yummy.nbt.getSortedInventory
 import yummyloop.yummy.registry.Register
 import java.util.function.Supplier
-import kotlin.math.ceil
 
 object ModContent {
     var EXAMPLE_ITEM =
@@ -70,6 +55,8 @@ object ModContent {
 
         BaHandler.rType = Register.screenHandlerTypeExtended("side_screen", ::BaHandler)
         Register.Client.screen(BaHandler.rType!!) { return@screen ::BaScreen }
+
+        ToolTipPreview // Initialize tooltip
     }
 
     /** Dev content */
@@ -78,227 +65,6 @@ object ModContent {
             G2
             G3
             //E1
-
-            Register.Client {
-                val client = MinecraftClient.getInstance()
-                var stack = ItemStack.EMPTY
-                val pressedKeyCode = "key.keyboard.left.shift"
-                var isKeyPressed = false
-                var invSize = 1
-
-                val shulkerList = mutableListOf("shulker_box")
-
-                fun getInv(s: ItemStack): Inventory? {
-                    //LOG.info(MinecraftClient.getInstance().player!!.enderChestInventory.getStack(0).item) // needs to be on the server side
-                    val tag: CompoundTag? = s.tag
-                    if (s != ItemStack.EMPTY && tag != null) {
-                        // Custom inventories
-                        var inv = tag.getSortedInventory()
-                        if (inv != null) return inv
-
-                        // Vanilla inventories
-                        val subTag = tag.getCompound("BlockEntityTag")
-                        if (!subTag.isEmpty) {
-                            inv = subTag.getSortedInventory()
-                            if (inv != null) return inv
-                        }
-                    }
-                    return null
-                }
-
-                /**
-                 * Draws an item
-                 *
-                 * @see HandledScreen
-                 */
-                fun renderItem(matrices: MatrixStack, itemStack: ItemStack, x: Int, y: Int) {
-                    val client = MinecraftClient.getInstance()
-                    val itemRenderer = client.itemRenderer
-                    val textRenderer = client.textRenderer
-
-                    val itemCountString : String = itemStack.count.let {
-                        if (it > 999) return@let "${itemStack.count/1000}k"
-                        if (it > 9999) return@let "+9k"
-                        return@let "${itemStack.count}"
-                    }
-
-                    matrices.push()
-                    itemRenderer.renderInGuiWithOverrides(itemStack, x, y)
-                    RenderSystem.pushMatrix()
-                    var scale = 1F
-                    if (itemStack.count in 100..999) {
-                        scale = 0.8F
-                        RenderSystem.translatef(1 / scale,1 / scale, 1F)
-                        RenderSystem.scalef(scale, scale, 1F)
-                    }
-                    itemRenderer.renderGuiItemOverlay(textRenderer, itemStack,
-                        ceil(x/scale).toInt(),
-                        ceil(y/scale).toInt(),
-                        itemCountString)
-                    RenderSystem.popMatrix()
-                    matrices.pop()
-                }
-
-                fun renderItems(matrices: MatrixStack, inv: Inventory, x: Int, y: Int) {
-                    val client = MinecraftClient.getInstance()
-                    val itemRenderer = client.itemRenderer
-                    val maxSize = 15
-
-                    matrices.push()
-                    fun renderI(col: Int) {
-                        for (i in 0 until invSize) {
-                            if (i >= maxSize - 1) break
-                            renderItem(matrices,
-                                inv.getStack(i),
-                                x + (i % col) * 18,
-                                y + (i / col) * 18)
-                        }
-                    }
-
-                    RenderSystem.translatef(0.0f, 0.0f, 32.0f)
-                    itemRenderer.zOffset = 200.0f
-
-                    when (invSize) {
-                        4 -> renderI(2)
-                        5 -> renderI(3)
-                        6 -> renderI(3)
-                        7 -> renderI(4)
-                        8 -> renderI(3)
-                        9 -> renderI(3)
-                        10 -> {
-                            for (i in 0 until 7) {
-                                renderItem(matrices,
-                                    inv.getStack(i),
-                                    x + (i % 4) * 18,
-                                    y + (i / 4) * 18)
-                            }
-                            for (i in 7 until 10) {
-                                renderItem(matrices,
-                                    inv.getStack(i),
-                                    x + ((i - 7) % 3) * 18,
-                                    y + ((i - 7) / 3) * 18 + 2 * 18)
-                            }
-                        }
-                        11 -> renderI(4)
-                        12 -> renderI(4)
-                        13 -> {
-                            for (i in 0 until 9) {
-                                renderItem(matrices,
-                                    inv.getStack(i),
-                                    x + (i % 5) * 18,
-                                    y + (i / 5) * 18)
-                            }
-                            for (i in 9 until 13) {
-                                renderItem(matrices,
-                                    inv.getStack(i),
-                                    x + ((i - 9) % 4) * 18,
-                                    y + ((i - 9) / 4) * 18 + 2 * 18)
-                            }
-                        }
-                        else -> renderI(5)
-                    }
-                    itemRenderer.zOffset = 0.0f
-                    matrices.pop()
-                }
-
-                fun renderBackground(matrices: MatrixStack, x: Int, y: Int) {
-                    matrices.push()
-                    client.textureManager.bindTexture(Identifier("yummy", "textures/gui/grid.png"))
-                    RenderSystem.translatef(0F, 0F, 400F)
-
-                    val offSetX = -8
-                    val offSetY = -8
-
-                    fun draw(xOffset: Int, yOffset: Int, u: Float, v: Float, width: Int, height: Int) =
-                        DrawableHelper.drawTexture(matrices,
-                            x + offSetX + xOffset,
-                            y + offSetY + yOffset,
-                            u,
-                            v,
-                            width,
-                            height,
-                            300,
-                            300)
-
-                    fun draw(u: Float, v: Float, width: Int, height: Int) = draw(0, 0, u, v, width, height)
-                    when (invSize) {
-                        1 -> draw(0F, 0F, 32, 32)
-                        2 -> draw(33F, 0F, 50, 32)
-                        3 -> draw(84F, 0F, 68, 32)
-                        4 -> draw(0F, 33F, 50, 50)
-                        5 -> draw(51F, 33F, 68, 50)
-                        6 -> draw(120F, 33F, 68, 50)
-                        7 -> draw(0F, 84F, 86, 50)
-                        8 -> draw(87F, 84F, 68, 68)
-                        9 -> draw(156F, 84F, 68, 68)
-                        10 -> draw(0F, 135F, 86, 68)
-                        11 -> draw(87F, 153F, 86, 68)
-                        12 -> draw(174F, 153F, 86, 68)
-                        13 -> {
-                            draw(105F, 223F, 79, 68)
-                            draw(79, 0, 61F, 135F, 25, 68)
-                        }
-                        14 -> draw(105F, 223F, 104, 68)
-                        else -> draw(0F, 223F, 104, 68)
-                    }
-
-                    matrices.pop()
-                }
-
-                TooltipEvent.ITEM.register { itemStack: ItemStack, lines: MutableList<Text>, tooltipContext: TooltipContext ->
-                    if (client.world != null) {
-                        stack = itemStack
-
-                        if (shulkerList.any { Regex(it).containsMatchIn(stack.item.translationKey) }) {
-                            lines.removeAll {
-                                if (it == lines.first()) {
-                                    //LOG.info("skipped: $it")
-                                    return@removeAll false
-                                }
-                                if (it is TranslatableText
-                                    && (it.toString().contains(Regex("minecraft|shulkerBox")))
-                                ) {
-                                    //LOG.info("removed : $it")
-                                    return@removeAll true
-                                }
-                                return@removeAll false
-                            }
-                        }
-                    }
-                }
-
-                TooltipEvent.RENDER_VANILLA_PRE.register { matrices: MatrixStack, lines: MutableList<out OrderedText>, x: Int, y: Int ->
-                    if (client.world != null && isKeyPressed) {
-                        val inv = getInv(stack)
-                        if (inv != null) {
-                            val offsetX = 14
-                            val offsetY = 3 + 10 * (lines.size - 1) + (if (lines.size > 1) 2 else 0) + 3
-
-                            val maxSize = 15
-                            invSize = if (inv.size() <= maxSize) inv.size() else maxSize
-
-                            renderBackground(matrices, x + offsetX, y + offsetY)
-                            renderItems(matrices, inv, x + offsetX, y + offsetY)
-                        }
-                    }
-                    stack= ItemStack.EMPTY
-                    return@register ActionResult.SUCCESS
-                }
-
-                ClientScreenInputEvent.KEY_PRESSED_PRE.register { client: MinecraftClient, screen: Screen, keyCode: Int, scanCode: Int, modifiers: Int ->
-                    if (InputUtil.fromTranslationKey(pressedKeyCode).code == keyCode) isKeyPressed = true
-                    return@register ActionResult.PASS
-                }
-                ClientScreenInputEvent.KEY_RELEASED_PRE.register { client: MinecraftClient, screen: Screen, keyCode: Int, scanCode: Int, modifiers: Int ->
-                    if (InputUtil.fromTranslationKey(pressedKeyCode).code == keyCode) isKeyPressed = false
-                    return@register ActionResult.PASS
-                }
-                return@Client Unit
-            }
-
-
-
-
 
             Eve.PLAYER_SCREEN_HANDLER_POST.register { playerInventory, onServer, playerEntity ->
                 val inv = SimpleInventory(2)
---
+++ common/src/main/yummyloop/yummy/client/ToolTipPreview.kt
@@ -0,0 +1,258 @@
+package yummyloop.yummy.client
+
+import com.mojang.blaze3d.systems.RenderSystem
+import me.shedaniel.architectury.event.events.TooltipEvent
+import me.shedaniel.architectury.event.events.client.ClientScreenInputEvent
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gui.DrawableHelper
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.item.TooltipContext
+import net.minecraft.client.util.InputUtil
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.inventory.Inventory
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.text.OrderedText
+import net.minecraft.text.Text
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.ActionResult
+import net.minecraft.util.Identifier
+import yummyloop.yummy.nbt.getSortedInventory
+import yummyloop.yummy.registry.Register
+import kotlin.math.ceil
+
+object ToolTipPreview {
+    init {
+        Register.Client { Client }
+    }
+
+    @Environment(EnvType.CLIENT)
+    private object Client {
+        var client: MinecraftClient = MinecraftClient.getInstance()
+        private var stack: ItemStack = ItemStack.EMPTY
+        private var pressedKeyCode: String = "key.keyboard.left.shift"
+        private var isKeyPressed: Boolean = false
+        private var invSize: Int = 1
+        private var shulkerList = mutableListOf("shulker_box")
+
+        init {
+            captureTooltip()
+            render()
+            captureKey()
+        }
+
+        fun captureTooltip() {
+            TooltipEvent.ITEM.register { itemStack: ItemStack, lines: MutableList<Text>, tooltipContext: TooltipContext ->
+                if (client.world != null) {
+                    stack = itemStack
+
+                    if (shulkerList.any { Regex(it).containsMatchIn(stack.item.translationKey) }) {
+                        lines.removeAll {
+                            if (it == lines.first()) {
+                                //LOG.info("skipped: $it")
+                                return@removeAll false
+                            }
+                            if (it is TranslatableText
+                                && (it.toString().contains(Regex("minecraft|shulkerBox")))
+                            ) {
+                                //LOG.info("removed : $it")
+                                return@removeAll true
+                            }
+                            return@removeAll false
+                        }
+                    }
+                }
+            }
+        }
+
+        fun render() {
+            TooltipEvent.RENDER_VANILLA_PRE.register { matrices: MatrixStack, lines: MutableList<out OrderedText>, x: Int, y: Int ->
+                if (client.world != null && isKeyPressed) {
+                    val inv = getInv(stack)
+                    if (inv != null) {
+                        val offsetX = 14
+                        val offsetY = 3 + 10 * (lines.size - 1) + (if (lines.size > 1) 2 else 0) + 3
+
+                        val maxSize = 15
+                        invSize = if (inv.size() <= maxSize) inv.size() else maxSize
+
+                        renderBackground(matrices, x + offsetX, y + offsetY)
+                        renderItems(matrices, inv, x + offsetX, y + offsetY)
+                    }
+                }
+                stack = ItemStack.EMPTY
+                return@register ActionResult.SUCCESS
+            }
+        }
+
+        fun captureKey() {
+            ClientScreenInputEvent.KEY_PRESSED_PRE.register { client: MinecraftClient, screen: Screen, keyCode: Int, scanCode: Int, modifiers: Int ->
+                if (InputUtil.fromTranslationKey(pressedKeyCode).code == keyCode) isKeyPressed = true
+                return@register ActionResult.PASS
+            }
+            ClientScreenInputEvent.KEY_RELEASED_PRE.register { client: MinecraftClient, screen: Screen, keyCode: Int, scanCode: Int, modifiers: Int ->
+                if (InputUtil.fromTranslationKey(pressedKeyCode).code == keyCode) isKeyPressed = false
+                return@register ActionResult.PASS
+            }
+        }
+
+        fun getInv(s: ItemStack): Inventory? {
+            //LOG.info(MinecraftClient.getInstance().player!!.enderChestInventory.getStack(0).item) // needs to be on the server side
+            val tag: CompoundTag? = s.tag
+            if (s != ItemStack.EMPTY && tag != null) {
+                // Custom inventories
+                var inv = tag.getSortedInventory()
+                if (inv != null) return inv
+
+                // Vanilla inventories
+                val subTag = tag.getCompound("BlockEntityTag")
+                if (!subTag.isEmpty) {
+                    inv = subTag.getSortedInventory()
+                    if (inv != null) return inv
+                }
+            }
+            return null
+        }
+
+        /**
+         * Draws an item
+         *
+         * @see HandledScreen
+         */
+        fun renderItem(matrices: MatrixStack, itemStack: ItemStack, x: Int, y: Int) {
+            val client = MinecraftClient.getInstance()
+            val itemRenderer = client.itemRenderer
+            val textRenderer = client.textRenderer
+
+            val itemCountString: String = itemStack.count.let {
+                if (it > 999) return@let "${itemStack.count / 1000}k"
+                if (it > 9999) return@let "+9k"
+                return@let "${itemStack.count}"
+            }
+
+            matrices.push()
+            itemRenderer.renderInGuiWithOverrides(itemStack, x, y)
+            RenderSystem.pushMatrix()
+            var scale = 1F
+            if (itemStack.count in 100..999) {
+                scale = 0.8F
+                RenderSystem.translatef(1 / scale, 1 / scale, 1F)
+                RenderSystem.scalef(scale, scale, 1F)
+            }
+            itemRenderer.renderGuiItemOverlay(textRenderer, itemStack,
+                ceil(x / scale).toInt(),
+                ceil(y / scale).toInt(),
+                itemCountString)
+            RenderSystem.popMatrix()
+            matrices.pop()
+        }
+
+        fun renderItems(matrices: MatrixStack, inv: Inventory, x: Int, y: Int) {
+            val client = MinecraftClient.getInstance()
+            val itemRenderer = client.itemRenderer
+            val maxSize = 15
+
+            matrices.push()
+            fun renderI(col: Int) {
+                for (i in 0 until invSize) {
+                    if (i >= maxSize - 1) break
+                    renderItem(matrices,
+                        inv.getStack(i),
+                        x + (i % col) * 18,
+                        y + (i / col) * 18)
+                }
+            }
+
+            RenderSystem.translatef(0.0f, 0.0f, 32.0f)
+            itemRenderer.zOffset = 200.0f
+
+            when (invSize) {
+                4 -> renderI(2)
+                5 -> renderI(3)
+                6 -> renderI(3)
+                7 -> renderI(4)
+                8 -> renderI(3)
+                9 -> renderI(3)
+                10 -> {
+                    for (i in 0 until 7) {
+                        renderItem(matrices,
+                            inv.getStack(i),
+                            x + (i % 4) * 18,
+                            y + (i / 4) * 18)
+                    }
+                    for (i in 7 until 10) {
+                        renderItem(matrices,
+                            inv.getStack(i),
+                            x + ((i - 7) % 3) * 18,
+                            y + ((i - 7) / 3) * 18 + 2 * 18)
+                    }
+                }
+                11 -> renderI(4)
+                12 -> renderI(4)
+                13 -> {
+                    for (i in 0 until 9) {
+                        renderItem(matrices,
+                            inv.getStack(i),
+                            x + (i % 5) * 18,
+                            y + (i / 5) * 18)
+                    }
+                    for (i in 9 until 13) {
+                        renderItem(matrices,
+                            inv.getStack(i),
+                            x + ((i - 9) % 4) * 18,
+                            y + ((i - 9) / 4) * 18 + 2 * 18)
+                    }
+                }
+                else -> renderI(5)
+            }
+            itemRenderer.zOffset = 0.0f
+            matrices.pop()
+        }
+
+        fun renderBackground(matrices: MatrixStack, x: Int, y: Int) {
+            matrices.push()
+            client.textureManager.bindTexture(Identifier("yummy", "textures/gui/grid.png"))
+            RenderSystem.translatef(0F, 0F, 400F)
+
+            val offSetX = -8
+            val offSetY = -8
+
+            fun draw(xOffset: Int, yOffset: Int, u: Float, v: Float, width: Int, height: Int) =
+                DrawableHelper.drawTexture(matrices,
+                    x + offSetX + xOffset,
+                    y + offSetY + yOffset,
+                    u,
+                    v,
+                    width,
+                    height,
+                    300,
+                    300)
+
+            fun draw(u: Float, v: Float, width: Int, height: Int) = draw(0, 0, u, v, width, height)
+            when (invSize) {
+                1 -> draw(0F, 0F, 32, 32)
+                2 -> draw(33F, 0F, 50, 32)
+                3 -> draw(84F, 0F, 68, 32)
+                4 -> draw(0F, 33F, 50, 50)
+                5 -> draw(51F, 33F, 68, 50)
+                6 -> draw(120F, 33F, 68, 50)
+                7 -> draw(0F, 84F, 86, 50)
+                8 -> draw(87F, 84F, 68, 68)
+                9 -> draw(156F, 84F, 68, 68)
+                10 -> draw(0F, 135F, 86, 68)
+                11 -> draw(87F, 153F, 86, 68)
+                12 -> draw(174F, 153F, 86, 68)
+                13 -> {
+                    draw(105F, 223F, 79, 68)
+                    draw(79, 0, 61F, 135F, 25, 68)
+                }
+                14 -> draw(105F, 223F, 104, 68)
+                else -> draw(0F, 223F, 104, 68)
+            }
+
+            matrices.pop()
+        }
+    }
+}
