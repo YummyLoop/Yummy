16af997e5fb34b621cda6842ed98712407692c95
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2021-03-17 16:32:46 +0000

    Add blockstate rotations

--- common/src/main/yummyloop/yummy/content/chest/ChestBlock.kt
+++ common/src/main/yummyloop/yummy/content/chest/ChestBlock.kt
@@ -1,25 +1,52 @@
 package yummyloop.yummy.content.chest
 
 import me.shedaniel.architectury.registry.MenuRegistry
-import net.minecraft.block.BlockRenderType
-import net.minecraft.block.BlockState
-import net.minecraft.block.BlockWithEntity
+import net.minecraft.block.*
+import net.minecraft.block.ChestBlock
 import net.minecraft.block.entity.BlockEntity
-import net.minecraft.block.entity.ChestBlockEntity
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.ItemPlacementContext
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.state.StateManager
+import net.minecraft.state.property.BooleanProperty
+import net.minecraft.state.property.DirectionProperty
+import net.minecraft.state.property.Properties
 import net.minecraft.util.ActionResult
+import net.minecraft.util.BlockMirror
+import net.minecraft.util.BlockRotation
 import net.minecraft.util.Hand
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
-import java.util.concurrent.TimeUnit
 
-open class ChestBlock(settings: Settings) : BlockWithEntity(settings) {
+open class ChestBlock(settings: Settings) : BlockWithEntity(settings), Waterloggable {
 
+    companion object {
+        val FACING: DirectionProperty = HorizontalFacingBlock.FACING
+        val WATERLOGGED: BooleanProperty = Properties.WATERLOGGED
+    }
     init {
+        defaultState = this.stateManager.defaultState
+            .with(FACING, Direction.NORTH)
+            .with(WATERLOGGED, false)
+    }
 
+    override fun getPlacementState(ctx: ItemPlacementContext): BlockState {
+        return this.defaultState.with(FACING, ctx.playerFacing.opposite)
+    }
+
+    override fun appendProperties(builder: StateManager.Builder<Block?, BlockState?>) {
+        builder.add(FACING, WATERLOGGED)
+    }
+
+    override fun rotate(state: BlockState, rotation: BlockRotation): BlockState? {
+        return state.with(FACING, rotation.rotate(state.get(FACING) as Direction)) as BlockState
+    }
+
+    override fun mirror(state: BlockState, mirror: BlockMirror): BlockState? {
+        return state.rotate(mirror.getRotation(state.get(FACING) as Direction))
     }
 
     override fun createBlockEntity(world: BlockView?): BlockEntity = ChestEntity()
