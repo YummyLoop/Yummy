957c9f8106de9400c7db71cf744b6f6188097ed2
YummyLoop <12478030+YummyLoop@users.noreply.github.com>
2021-03-17 23:50:14 +0000

    add block drops

--- common/src/main/yummyloop/yummy/content/chest/ChestBlock.kt
+++ common/src/main/yummyloop/yummy/content/chest/ChestBlock.kt
@@ -3,19 +3,19 @@ package yummyloop.yummy.content.chest
 import me.shedaniel.architectury.registry.MenuRegistry
 import net.minecraft.block.*
 import net.minecraft.block.entity.BlockEntity
+import net.minecraft.entity.ai.pathing.NavigationType
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.fluid.FluidState
 import net.minecraft.fluid.Fluids
+import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemPlacementContext
+import net.minecraft.screen.ScreenHandler
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.state.StateManager
 import net.minecraft.state.property.BooleanProperty
 import net.minecraft.state.property.DirectionProperty
 import net.minecraft.state.property.Properties
-import net.minecraft.util.ActionResult
-import net.minecraft.util.BlockMirror
-import net.minecraft.util.BlockRotation
-import net.minecraft.util.Hand
+import net.minecraft.util.*
 import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
@@ -97,5 +97,39 @@ open class ChestBlock(settings: Settings) : BlockWithEntity(settings), Waterlogg
         }
     }
 
+    /** On break, replace, ... */
+    override fun onStateReplaced(
+        state: BlockState,
+        world: World,
+        pos: BlockPos?,
+        newState: BlockState,
+        moved: Boolean,
+    ) {
+        if (!state.isOf(newState.block)) {
+            val blockEntity = world.getBlockEntity(pos)
+            if (blockEntity is Inventory) {
+                ItemScatterer.spawn(world, pos, blockEntity)
+                world.updateComparators(pos, this)
+            }
+            super.onStateReplaced(state, world, pos, newState, moved)
+        }
+    }
 
+    override fun hasComparatorOutput(state: BlockState?): Boolean = true
+
+    override fun getComparatorOutput(state: BlockState?, world: World?, pos: BlockPos?): Int {
+        val blockEntity = world!!.getBlockEntity(pos)
+        return if (blockEntity is Inventory) {
+            ScreenHandler.calculateComparatorOutput(blockEntity as Inventory)
+        } else ScreenHandler.calculateComparatorOutput(null as Inventory?)
+    }
+
+    override fun canPathfindThrough(
+        state: BlockState?,
+        world: BlockView?,
+        pos: BlockPos?,
+        type: NavigationType?,
+    ): Boolean {
+        return false
+    }
 }
